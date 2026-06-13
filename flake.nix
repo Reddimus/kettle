aa008c82c4aa4767e6b6f551df527f640fc07809
kettle <kevi.m.cecs@gmail.com>
2026-06-13 15:42:06 -0700

    release: v2.22.0
    See CHANGELOG.md [2.22.0].

--- flake.nix
+++ flake.nix
@@ -45,7 +45,7 @@
           pname = "kettle";
           # Keep in lockstep with `Cargo.toml`'s workspace `version`.
           # Bump in the same PR that bumps the release tag.
-          version = "2.21.1";
+          version = "2.22.0";
           src = ./.;
 
           cargoLock = {
