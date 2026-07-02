97a9dfbaa19540d48bfc8b378cbb5d67897cfda2
KeviM <Kevin.MartinezLopez@zii.aero>
2026-07-02 14:55:07 -0700

    release: v2.34.0
    See CHANGELOG.md [2.34.0].

--- flake.nix
+++ flake.nix
@@ -45,7 +45,7 @@
           pname = "kettle";
           # Keep in lockstep with `Cargo.toml`'s workspace `version`.
           # Bump in the same PR that bumps the release tag.
-          version = "2.33.1";
+          version = "2.34.0";
           src = ./.;
 
           cargoLock = {
